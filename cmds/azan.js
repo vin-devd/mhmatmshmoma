276f3779a8a4099f18b2eb6726b60498f420e60c
VIN DEVD <152078516+vin-devd@users.noreply.github.com>
2024-09-30 21:38:05 +0300

    Add files via upload

---
+++ cmds/azan.js
@@ -0,0 +1,149 @@
+const { PrayerTimes, CalculationMethod } = require('adhan');
+const mongoose = require('mongoose');
+const fileSchema = require('../schema/azanSchema');
+const moment = require('moment-timezone');
+const { PermissionsBitField, SlashCommandBuilder, ChannelType, EmbedBuilder, Colors, ButtonBuilder, ButtonStyle, ActionRowBuilder } = require('discord.js');
+
+const countries = {
+    'مصر': { lat: 30.033333, lon: 31.233334, timezone: 'Africa/Cairo' },
+    'السعودية': { lat: 24.7136, lon: 46.6753, timezone: 'Asia/Riyadh' },
+    'الإمارات': { lat: 25.276987, lon: 55.296249, timezone: 'Asia/Dubai' },
+    'الجزائر': { lat: 36.737232, lon: 3.086472, timezone: 'Africa/Algiers' },
+    'الأردن': { lat: 31.963158, lon: 35.930359, timezone: 'Asia/Amman' },
+    'المغرب': { lat: 31.791702, lon: -7.09262, timezone: 'Africa/Casablanca' },
+    'العراق': { lat: 33.3152, lon: 44.3661, timezone: 'Asia/Baghdad' },
+    'الكويت': { lat: 29.3759, lon: 47.9774, timezone: 'Asia/Kuwait' },
+    'قطر': { lat: 25.276987, lon: 51.521042, timezone: 'Asia/Qatar' },
+    'تونس': { lat: 36.806389, lon: 10.181667, timezone: 'Africa/Tunis' },
+    'البحرين': { lat: 26.0667, lon: 50.5577, timezone: 'Asia/Bahrain' },
+    'لبنان': { lat: 33.8547, lon: 35.8623, timezone: 'Asia/Beirut' },
+    'السودان': { lat: 15.5007, lon: 32.5599, timezone: 'Africa/Khartoum' },
+    'ليبيا': { lat: 32.8872, lon: 13.1913, timezone: 'Africa/Tripoli' },
+    'فلسطين': { lat: 31.9522, lon: 35.2332, timezone: 'Asia/Gaza' },
+    'سوريا': { lat: 33.5138, lon: 36.2765, timezone: 'Asia/Damascus' },
+    'عمان': { lat: 23.6100, lon: 58.5400, timezone: 'Asia/Muscat' },
+    'موريتانيا': { lat: 18.0735, lon: -15.9582, timezone: 'Africa/Nouakchott' },
+    'الصومال': { lat: 5.1521, lon: 46.1996, timezone: 'Africa/Mogadishu' },
+    'جيبوتي': { lat: 11.8251, lon: 42.5903, timezone: 'Africa/Djibouti' },
+    'جزر القمر': { lat: -11.6455, lon: 43.3333, timezone: 'Indian/Comoro' },
+    'اليمن': { lat: 15.5527, lon: 48.5164, timezone: 'Asia/Aden' },
+    'United Kingdom': { lat: 51.5074, lon: -0.1278, timezone: 'Europe/London' },
+    'Germany': { lat: 52.52, lon: 13.405, timezone: 'Europe/Berlin' },
+    'Pakistan': { lat: 24.8607, lon: 67.0011, timezone: 'Asia/Karachi' },
+};
+
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('ضبط')
+        .addSubcommand(S => S.setName('الأذان').setDescription('تعيين قناة لإرسال إشعارات الصلاة')
+        .setDescription('تعيين قناة لإرسال إشعارات الصلاة')
+        .addStringOption(option =>
+            option.setName('country')
+                .setDescription('اختر دولتك للحصول على مواعيد الصلاة')
+                .setRequired(true)
+                .addChoices(
+                    { name: 'مصر', value: 'مصر' },
+                    { name: 'السعودية', value: 'السعودية' },
+                    { name: 'الإمارات', value: 'الإمارات' },
+                    { name: 'الجزائر', value: 'الجزائر' },
+                    { name: 'الأردن', value: 'الأردن' },
+                    { name: 'المغرب', value: 'المغرب' },
+                    { name: 'العراق', value: 'العراق' },
+                    { name: 'الكويت', value: 'الكويت' },
+                    { name: 'قطر', value: 'قطر' },
+                    { name: 'تونس', value: 'تونس' },
+                    { name: 'البحرين', value: 'البحرين' },
+                    { name: 'لبنان', value: 'لبنان' },
+                    { name: 'السودان', value: 'السودان' },
+                    { name: 'ليبيا', value: 'ليبيا' },
+                    { name: 'فلسطين', value: 'فلسطين' },
+                    { name: 'سوريا', value: 'سوريا' },
+                    { name: 'عمان', value: 'عمان' },
+                    { name: 'موريتانيا', value: 'موريتانيا' },
+                    { name: 'الصومال', value: 'الصومال' },
+                    { name: 'جيبوتي', value: 'جيبوتي' },
+                    { name: 'جزر القمر', value: 'جزر القمر' },
+                    { name: 'اليمن', value: 'اليمن' },
+                    { name: 'United Kingdom', value: 'United Kingdom' },
+                    { name: 'Germany', value: 'Germany' },
+                    { name: 'Pakistan', value: 'Pakistan' }
+                ))
+        .addChannelOption(option =>
+            option.setName('channel')
+                .setDescription('اختر القناة التي تريد تعيينها')
+                .addChannelTypes(ChannelType.GuildText, ChannelType.GuildAnnouncement)
+                .setRequired(false))),
+
+    execute: async (int) => {
+        await int.deferReply();
+        const channel = int.options.getChannel('channel') || int.channel;
+        const country = int.options.getString('country');
+        const bot = int.guild.members.me;
+        const { lat, lon, timezone } = countries[country];
+
+        if (!int.member.permissions.has(PermissionsBitField.Flags.ManageChannels)) {
+            return int.editReply({ content: '❌ لا تملك صلاحيات كافية لإجراء هذا الأمر!' });
+        }
+
+        if (!bot.permissionsIn(channel).has(PermissionsBitField.Flags.ViewChannel)) {
+            return int.editReply({ content: '❌ لا أستطيع رؤية هذه القناة!' });
+        }
+        if(!int.guild) return;
+        const dataFile = await fileSchema.findOne({ guildId: int.guild.id });
+        if (dataFile) {
+            const embed = new EmbedBuilder()
+                .setTitle('⚠️ يوجد بيانات الخادم بالفعل')
+                .setDescription('هل تريد تحديث البيانات أم حذفها؟')
+                .setColor(Colors.DarkerGrey)
+                .setThumbnail(int.guild.iconURL())
+                .setFooter({ text: 'يرجى اتخاذ إجراء.' });
+
+            const buttonUpdate = new ButtonBuilder()
+                .setCustomId(`updateButton_${channel.id}_${country}_${lon}_${lat}`)
+                .setLabel('تحديث')
+                .setStyle(ButtonStyle.Primary);
+
+            const buttonDelete = new ButtonBuilder()
+                .setCustomId('deleteButton')
+                .setLabel('حذف')
+                .setStyle(ButtonStyle.Danger);
+
+            const actionRow = new ActionRowBuilder()
+                .addComponents(buttonDelete, buttonUpdate);
+
+            await int.deleteReply();
+            return await int.followUp({ embeds: [embed], ephemeral: true, components: [actionRow] });
+        }
+
+        function pray(coordinates, timezone) {
+            const date = moment().tz(timezone).toDate();
+            const params = CalculationMethod.MuslimWorldLeague();
+            const prayerTimes = new PrayerTimes(coordinates, date, params);
+
+            return {
+                fajr: moment(prayerTimes.fajr).tz(timezone).format('HH:mm A'),
+                dhuhr: moment(prayerTimes.dhuhr).tz(timezone).format('HH:mm A'),
+                asr: moment(prayerTimes.asr).tz(timezone).format('HH:mm A'),
+                maghrib: moment(prayerTimes.maghrib).tz(timezone).format('HH:mm A'),
+                isha: moment(prayerTimes.isha).tz(timezone).format('HH:mm A')
+            };
+        }
+
+        const coordinates = { latitude: lat, longitude: lon };
+        const times = pray(coordinates, timezone);
+
+        const saveData = new fileSchema({
+            guildId: int.guild.id,
+            country: country,
+            channelId: channel.id,
+            lat: lat,
+            lon: lon
+        });
+
+        await saveData.save();
+
+        await int.deleteReply();
+        return await int.followUp({ content: `✅ تم إعداد القناة ${channel} لعرض مواعيد الصلاة لـ ${country}.` });
+    }
+};
---
+++ main.js
@@ -0,0 +1,253 @@
+const { Client, ActivityType, Collection, ButtonBuilder, ButtonStyle, ActionRowBuilder, EmbedBuilder, Colors } = require('discord.js');
+require('dotenv').config();
+const token = process.env.TOKEN;
+const clientId = process.env.CLIENTID;
+const { REST } = require('@discordjs/rest');
+const mongoose = require('mongoose');
+const { Routes } = require('discord-api-types/v9');
+const { log } = require('console');
+const { readdirSync } = require('fs');
+const fileSchema = require('./schema/azanSchema');
+const moment = require('moment-timezone');
+const { PrayerTimes, CalculationMethod } = require('adhan');
+
+mongoose.connect('mongodb+srv://dfhh:YeqlAeFzhChutTbL@id.wy06g.mongodb.net/', {}).then(() => {
+    console.log('تم الاتصال بقاعدة البيانات بنجاح!');
+});
+
+const bot = new Client({
+    intents: 65123,
+});
+bot.login(token);
+const commands = [];
+bot.commands = new Collection();
+const files = readdirSync('./cmds').filter(f => f.endsWith('.js'));
+
+for (let file of files) {
+    const command = require(`./cmds/${file}`);
+    commands.push(command.data.toJSON());
+    bot.commands.set(command.data.name, command);
+}
+
+(async () => {
+    try {
+        const rest = new REST({ version: "9" }).setToken(token);
+        await rest.put(Routes.applicationCommands(clientId), { body: commands });
+        log('تم تحميل الأوامر بنجاح.');
+    } catch (Err) {
+        log(Err);
+    }
+})();
+
+bot.on('interactionCreate', async (int) => {
+    const command = bot.commands.get(int.commandName);
+    if (int.isCommand() && command) {
+        try {
+            await command.execute(int, bot);
+        } catch (err) {
+            log('حدث خطأ أثناء تنفيذ الأمر:', err);
+        }
+    }
+});
+
+bot.on('ready', async () => {
+    bot.user.setPresence({ activities: [{ name: 'Go...', type: ActivityType.Watching }], status: 'dnd' });
+    log('البوت جاهز للعمل!');
+});
+
+bot.on('interactionCreate', async (int) => {
+    if (int.isButton()) {
+        const DataSchema = require('./schema/azanSchema');
+
+        if (int.customId.startsWith('updateButton_')) {
+            const channelId = int.customId.split('_')[1];
+            const country = int.customId.split('_')[2];
+            const lona = int.customId.split('_')[3]
+            const lata = int.customId.split('_')[4]
+            const channel = bot.channels.cache.get(channelId);
+            const data = await DataSchema.findOne({ guildId: int.guildId });
+
+            const ButtonUpdate = new ButtonBuilder()
+                .setCustomId(`updateButton`)
+                .setLabel('تحديث المعلومات')
+                .setDisabled(true)
+                .setStyle(ButtonStyle.Primary);
+
+            const ButtonDelete = new ButtonBuilder()
+                .setCustomId('deleteButton')
+                .setLabel('حذف المعلومات')
+                .setDisabled(true)
+                .setStyle(ButtonStyle.Danger);
+
+            const actionRow = new ActionRowBuilder()
+                .addComponents(ButtonDelete, ButtonUpdate);
+
+            if (data) {
+                await int.update({ components: [actionRow], content: '✅ تم تحديث المعلومات بنجاح!' });
+                await DataSchema.findOneAndUpdate({ channelId: channelId, country: country, lat: `${lata}`, lon: `${lona}` });
+            }
+        } else if (int.customId === 'deleteButton') {
+            const data = await DataSchema.findOne({ guildId: int.guildId });
+
+            const ButtonUpdate = new ButtonBuilder()
+                .setCustomId(`updateButton`)
+                .setLabel('تحديث المعلومات')
+                .setDisabled(true)
+                .setStyle(ButtonStyle.Primary);
+
+            const ButtonDelete = new ButtonBuilder()
+                .setCustomId('deleteButton')
+                .setLabel('حذف المعلومات')
+                .setDisabled(true)
+                .setStyle(ButtonStyle.Danger);
+
+            const actionRow = new ActionRowBuilder()
+                .addComponents(ButtonDelete, ButtonUpdate);
+
+            if (data) {
+                await int.update({ components: [actionRow], content: '✅ تم حذف المعلومات بنجاح!' });
+                await DataSchema.deleteOne({ guildId: int.guildId, channelId: data.channelId, country: data.country });
+            }
+        }
+        if(int.customId === 'snn'){
+            
+const adhanEmbed = new EmbedBuilder()
+.setTitle('سنن الأذان للمصلي في الإسلام')
+.setDescription(`
+1. **الإجابة بعد الأذان**: يستحب ترديد ما يقول المؤذن إلا في "حي على الصلاة" و"حي على الفلاح"، فيقول "لا حول ولا قوة إلا بالله".
+
+2. **الدعاء بين الأذان والإقامة**: يستحب للمسلم الدعاء بين الأذان والإقامة، لأنه وقت إجابة الدعاء.
+
+3. **الصلاة على النبي ﷺ**: بعد انتهاء الأذان، يُستحب قول: "اللهم صل على محمد وعلى آل محمد كما صليت على إبراهيم..." إلى نهاية الصلاة الإبراهيمية.
+
+4. **الدعاء بعد الأذان**: بعد الصلاة على النبي، يُستحب قول الدعاء: "اللهم رب هذه الدعوة التامة والصلاة القائمة آتِ محمدًا الوسيلة والفضيلة وابعثه مقامًا محمودًا الذي وعدته".
+
+5. **الوضوء قبل الصلاة**: يُستحب أن يكون المسلم على وضوء قبل إقامة الصلاة.
+
+6. **الذهاب إلى المسجد بهدوء**: يُستحب الذهاب إلى المسجد بهدوء ووقار عند سماع الأذان.`)
+.setColor(Colors.DarkerGrey)
+.setFooter({text: 'الأذان تذكرة لوقت الصلاة وأوقات الإجابة.', iconURL: int.guild.iconURL()})
+.setTimestamp(); 
+
+  int.reply({embeds: [adhanEmbed], ephemeral:true})
+        }
+        if(int.customId === 'ad3ea'){
+          
+const duaEmbed = new EmbedBuilder()
+.setTitle('أدعية بين الأذان والإقامة')
+.setDescription(`يُستحب للمسلم الدعاء بين الأذان والإقامة، وهذا وقتٌ تُستجاب فيه الدعوات. إليك بعض الأدعية التي يمكنك قولها:
+
+1. **اللهم إني أسألك العفو والعافية في الدنيا والآخرة**.
+
+2. **اللهم اغفر لي ولوالدي، وللمؤمنين والمؤمنات، الأحياء منهم والأموات**.
+
+3. **اللهم إني أسألك من فضلك ورحمتك، فإنه لا يملكها إلا أنت**.
+
+4. **اللهم آتنا في الدنيا حسنة وفي الآخرة حسنة وقنا عذاب النار**.
+
+5. **اللهم إني أسألك الجنة وما قرب إليها من قولٍ أو عمل، وأعوذ بك من النار وما قرب إليها من قولٍ أو عمل**.
+
+6. **اللهم ارحمني واهدني واجبرني وعافني وارزقني**.
+
+7. **اللهم ارزقني حبك، وحب من يحبك، وحب عمل يقربني إلى حبك**.
+
+8. **اللهم إني أسألك الهدى والتقى والعفاف والغنى**.
+
+9. **اللهم اجعلني لك شكّارًا، لك ذَكّارًا، لك رهّابًا، لك مطواعًا، لك مخبتًا، إليك أواهًا منيبًا**.`)
+.setColor(Colors.DarkerGrey)
+.setFooter({text: 'الدعاء بين الأذان والإقامة مستحب ويُستجاب بإذن الله', iconURL: int.guild.iconURL()})
+.setTimestamp();
+int.reply({embeds: [duaEmbed], ephemeral:true})
+
+        }
+    }
+});
+
+const countries = {
+    'مصر': { lat: 30.033333, lon: 31.233334, timezone: 'Africa/Cairo' },
+    'السعودية': { lat: 24.7136, lon: 46.6753, timezone: 'Asia/Riyadh' },
+    'الإمارات': { lat: 25.276987, lon: 55.296249, timezone: 'Asia/Dubai' },
+    'الجزائر': { lat: 36.737232, lon: 3.086472, timezone: 'Africa/Algiers' },
+    'الأردن': { lat: 31.963158, lon: 35.930359, timezone: 'Asia/Amman' },
+    'المغرب': { lat: 31.791702, lon: -7.09262, timezone: 'Africa/Casablanca' },
+    'العراق': { lat: 33.3152, lon: 44.3661, timezone: 'Asia/Baghdad' },
+    'الكويت': { lat: 29.3759, lon: 47.9774, timezone: 'Asia/Kuwait' },
+    'قطر': { lat: 25.276987, lon: 51.521042, timezone: 'Asia/Qatar' },
+    'تونس': { lat: 36.806389, lon: 10.181667, timezone: 'Africa/Tunis' },
+    'البحرين': { lat: 26.0667, lon: 50.5577, timezone: 'Asia/Bahrain' },
+    'لبنان': { lat: 33.8547, lon: 35.8623, timezone: 'Asia/Beirut' },
+    'السودان': { lat: 15.5007, lon: 32.5599, timezone: 'Africa/Khartoum' },
+    'ليبيا': { lat: 32.8872, lon: 13.1913, timezone: 'Africa/Tripoli' },
+    'فلسطين': { lat: 31.9522, lon: 35.2332, timezone: 'Asia/Gaza' },
+    'سوريا': { lat: 33.5138, lon: 36.2765, timezone: 'Asia/Damascus' },
+    'عمان': { lat: 23.6100, lon: 58.5400, timezone: 'Asia/Muscat' },
+    'موريتانيا': { lat: 18.0735, lon: -15.9582, timezone: 'Africa/Nouakchott' },
+    'الصومال': { lat: 5.1521, lon: 46.1996, timezone: 'Africa/Mogadishu' },
+    'جيبوتي': { lat: 11.8251, lon: 42.5903, timezone: 'Africa/Djibouti' },
+    'جزر القمر': { lat: -11.6455, lon: 43.3333, timezone: 'Indian/Comoro' },
+    'اليمن': { lat: 15.5527, lon: 48.5164, timezone: 'Asia/Aden' },
+    'United Kingdom': { lat: 51.5074, lon: -0.1278, timezone: 'Europe/London' },
+    'Germany': { lat: 52.52, lon: 13.405, timezone: 'Europe/Berlin' },
+    'Pakistan': { lat: 24.8607, lon: 67.0011, timezone: 'Asia/Karachi' },
+};
+
+function calculatePrayerTimes(coordinates, timezone) {
+    const date = new Date();
+    const params = CalculationMethod.MuslimWorldLeague();
+    const prayerTimes = new PrayerTimes(coordinates, date, params);
+
+    return {
+        الفجر: moment(prayerTimes.fajr).tz(timezone).format('HH:mm'),
+        الظهر: moment(prayerTimes.dhuhr).tz(timezone).format('HH:mm'),
+        العصر: moment(prayerTimes.asr).tz(timezone).format('HH:mm'),
+        المغرب: moment(prayerTimes.maghrib).tz(timezone).format('HH:mm'),
+        العشاء: moment(prayerTimes.isha).tz(timezone).format('HH:mm'),
+    };
+}
+
+setInterval(async () => {
+    const guilds = await fileSchema.find();
+
+    guilds.forEach(async (dataFile) => {
+        const { guildId, channelId, country, lat, lon } = dataFile;
+        const guild = bot.guilds.cache.get(guildId);
+        const channel = bot.channels.cache.get(channelId);
+
+        if (channel) {
+            const timezone = countries[country]?.timezone;
+            if (!timezone) {
+                console.error(`الدولة "${country}" غير معرفة في القائمة.`);
+                return;
+            }
+
+            const prayerTimes = calculatePrayerTimes({ latitude: lat, longitude: lon }, timezone);
+            const currentTime = moment().tz(timezone).format('HH:mm');
+            const prayerKeys = Object.keys(prayerTimes);
+
+            for (const prayer of prayerKeys) {
+                if (currentTime === prayerTimes[prayer]) {
+                    const embedAzan = new EmbedBuilder()
+                    .setTitle(`موعد اذان ${prayer}`)
+                    .setDescription(`حان الان موعد اذان ${prayer} في دولة ${country}`)
+                    .setColor(Colors.Aqua)
+                    .setTimestamp()
+
+                    const ad3ea = new ButtonBuilder()
+                    .setCustomId('ad3ea')
+                    .setLabel('ادعية بعد الاذان')
+                    .setStyle(ButtonStyle.Secondary)
+                    
+                    
+                    const snn = new ButtonBuilder()
+                    .setCustomId('snn')
+                    .setLabel('سنن الاذان')
+                    .setStyle(ButtonStyle.Secondary)
+
+                    const acta = new ActionRowBuilder()
+                    .setComponents(snn, ad3ea)
+                    channel.send({embeds: [embedAzan], components: [acta]});
+                }
+            }
+        }
+    });
+}, 60000); 
